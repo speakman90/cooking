ab8f30dac3c3b16390f6cbdfbec6f52dedd92ea4
Pierre AS Speakman <97603002+speakman90@users.noreply.github.com>
2022-05-28 17:00:12 +0200

    add data on site

--- src/Controller/RecettesController.php
+++ src/Controller/RecettesController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Recette;
+use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/recettes', name: 'recettes_')]
 class RecettesController extends AbstractController
 {
-    #[Route('/recettes', name: 'app_recettes')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(RecetteRepository $recetteRepository): Response
     {
-        return $this->render('recettes/index.html.twig', [
-            'controller_name' => 'RecettesController',
+        return $this->render('recettes/index.html.twig',[
+            'recette' => $recetteRepository->findBy([],['title' => 'ASC']),
         ]);
+    }
+
+    #[Route('/{id}', name: 'details')]
+    public function details(Recette $recette): Response
+    {
+        return $this->render('recettes/detail.html.twig', compact('recette'));
     }
 }
