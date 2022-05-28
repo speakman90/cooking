b36b805a5c1a0b902c5cb49017efd321e8b15b31
Pierre AS Speakman <97603002+speakman90@users.noreply.github.com>
2022-05-28 12:40:52 +0200

    Add bootstrap

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 require('vue')
-
+require('bootstrap');
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
