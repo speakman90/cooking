b624bb7519dcb9f2b5eedb2977f5a6475f91ce71
Pierre AS Speakman <97603002+speakman90@users.noreply.github.com>
2022-06-04 13:18:23 +0200

    restart

--- assets/js/vue.js
+++
@@ -1,7 +0,0 @@
-const vue = new Vue({
-    mounted(){
-        axios.get("src/Controller/RecettesController.php")
-            .then((res) => res.data)
-            .then((res) => console.log('test'))
-    },
-}).$mount("#vue-app");
