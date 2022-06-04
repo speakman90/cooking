b5dd842831112fe305627f5e64a0ea361edffa60
Pierre AS Speakman <97603002+speakman90@users.noreply.github.com>
2022-06-04 17:33:50 +0200

    Add style, and forms for connection

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,9 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/index.less';
+import './styles/reset.css';
 
 // start the Stimulus application
 import './bootstrap';
+import './script.js';
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('script', './assets/script.js')
+    .addEntry('index', './assets/styles/index.less')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -57,6 +59,7 @@ Encore
 
     // enables Sass/SCSS support
     //.enableSassLoader()
+    .enableLessLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
